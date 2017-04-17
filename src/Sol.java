928413357865f6d8e3351bba1fa260b24b2e5e53
RadaevValentin <valentinradaev@gmail.com>
2017-04-17 09:31:14 +0300

    Result

--- src/Sol.java
+++ src/Sol.java
@@ -5,42 +5,21 @@ import java.util.*;
  * 1. Реализовать удаление дубликатов в списке.
  */
 public class Sol {
-    public List<Object> getList() {
-        return list;
-    }
-
     private List<Object> list = new ArrayList<>();
-
     public void initialize(Object ... data) {
         List<Object> objects = Arrays.asList(data);
         for(Object o : objects){
             list.add(o);
         }
     }
-
     public void printList(){
         this.list.forEach(System.out::println);
     }
-
-//    public void delCopy(){
-//        Set<Object> set = new HashSet<>(list);
-//        list.clear();
-//        list.addAll(set);
-//    } // если порядок добавления элементов не важен
-
     public void delCopy(){
-        Object a = null;
-        for(int i = 0; i < list.size(); i ++){
-            a = list.get(i);
-            for(int j = i+1; j< list.size(); j++){
-                if(a.equals(list.get(j))){
-                    list.remove(j);
-                }
-            }
-        }
-    } // если порядок добавления элементов важен
-
-
+        Set<Object> set = new LinkedHashSet<>(list);
+        list.clear();
+        list.addAll(set);
+    }
     public static void main(String[] args) {
     Sol sol = new Sol();
         sol.initialize("q", "w", 2, 3, 4, 2, 3, 4, 1, "q", "x", "q", 6, 4, 1, "w", "a", 546, "%", "a", 546);
@@ -48,4 +27,5 @@ public class Sol {
         sol.printList();
     }
 }
+
 
